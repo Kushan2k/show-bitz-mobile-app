912da8ac9490ad0f143ae7546e490b3b43eb9941
kushan2k <kushangayantha001@gmail.com>
2023-07-10 13:37:25 +0530

    movie fetching done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,6 +6,7 @@ import 'package:show_bitz/utils/constants.dart';
 import 'package:show_bitz/utils/styles.dart';
 import 'package:http/http.dart' as http;
 import 'package:show_bitz/widgets/movie_card.dart';
+// import 'package:show_bitz/widgets/movie_card.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,7 +16,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  Future<Map<String, dynamic>> loadMovies() async {
+  Future<List> loadMovies() async {
     const url =
         "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
 
@@ -24,49 +25,156 @@ class _HomeScreenState extends State<HomeScreen> {
       HttpHeaders.authorizationHeader: authToken,
     });
 
-    dynamic resp = jsonDecode(response.body);
+    List<dynamic> resp = jsonDecode(response.body)['results'];
+
+    return resp;
+  }
+
+  Future<List> loadPopularMovies() async {
+    const url =
+        "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+
+    var response = await http.get(Uri.parse(url), headers: <String, String>{
+      HttpHeaders.acceptHeader: 'application/json',
+      HttpHeaders.authorizationHeader: authToken,
+    });
+
+    List<dynamic> resp = jsonDecode(response.body)['results'];
 
     return resp;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 15,
-          vertical: 20,
-        ),
-        child: FutureBuilder(
-          future: loadMovies(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return SizedBox(
-                height: 120,
-                child: ListView.builder(
-                  itemBuilder: (context, index) {
-                    var item = snapshot.data?['results'];
-                    print(item);
-                    print('----------------------------------');
-                    // return MovieCard(title: title,);
-                    return const Text(
-                      "Movie",
-                    );
-                  },
-                  itemCount: snapshot.data?['results'].length,
-                  scrollDirection: Axis.horizontal,
-                ),
-              );
-            }
-
-            return Column(
+    return SingleChildScrollView(
+      child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 15,
+            vertical: 20,
+          ),
+          child: Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   "Now Playing",
                   style: headerStyle,
-                )
+                ),
+                FutureBuilder(
+                  future: loadMovies(),
+                  builder: (context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.done:
+                        return SizedBox(
+                          height: 300,
+                          child: ListView.builder(
+                            itemCount: snapshot.data?.length,
+                            itemBuilder: (context, index) {
+                              String imgPath =
+                                  snapshot.data?[index]['poster_path'];
+                              String url =
+                                  'https://image.tmdb.org/t/p/original$imgPath';
+                              String title = snapshot.data?[index]['title'];
+
+                              return MovieCard(
+                                title: title,
+                                img: url,
+                                index: index,
+                              );
+                            },
+                            scrollDirection: Axis.horizontal,
+                          ),
+                        );
+                      case ConnectionState.waiting:
+                        return showProgressIndicator();
+
+                      default:
+                        return showProgressIndicator();
+                    }
+                  },
+                ),
+                Text(
+                  "Popular",
+                  style: headerStyle,
+                ),
+                FutureBuilder(
+                  future: loadPopularMovies(),
+                  builder: (context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.done:
+                        return SizedBox(
+                          height: 300,
+                          child: ListView.builder(
+                            itemCount: snapshot.data?.length,
+                            itemBuilder: (context, index) {
+                              String imgPath =
+                                  snapshot.data?[index]['poster_path'];
+                              String url =
+                                  'https://image.tmdb.org/t/p/original$imgPath';
+                              String title = snapshot.data?[index]['title'];
+
+                              return MovieCard(
+                                title: title,
+                                img: url,
+                                index: index,
+                              );
+                            },
+                            scrollDirection: Axis.horizontal,
+                          ),
+                        );
+                      case ConnectionState.waiting:
+                        return showProgressIndicator();
+
+                      default:
+                        return showProgressIndicator();
+                    }
+                  },
+                ),
               ],
-            );
-          },
-        ));
+            ),
+          )),
+    );
   }
 }
+
+Widget showProgressIndicator() {
+  return const SizedBox(
+    height: 300,
+    child: Center(
+      child: Padding(
+        padding: EdgeInsets.only(
+          top: 40,
+        ),
+        child: CircularProgressIndicator(
+          color: Colors.white,
+          strokeWidth: 2,
+        ),
+      ),
+    ),
+  );
+}
+
+// ListView.builder(
+//                           itemBuilder: (context, index) {
+//                             // print(snapshot.data);
+
+//                             // print(title);
+//                             // print(item[index]);
+//                             // print('----------------------------------');
+//                             // return MovieCard(
+//                             //   title: title,
+//                             // );
+//                             return const Padding(
+//                               padding: EdgeInsets.all(15),
+//                               child: Text(
+//                                 "hello",
+//                                 style: TextStyle(
+//                                   color: Colors.white,
+//                                 ),
+//                               ),
+//                             );
+//                           },
+//                           // itemCount: snapshot.data?['results'].length,
+//                           itemCount: snapshot.data?.length,
+//                           scrollDirection: Axis.horizontal,
+//                         ),
--- lib/widgets/movie_card.dart
+++ lib/widgets/movie_card.dart
@@ -2,20 +2,59 @@ import 'package:flutter/material.dart';
 
 class MovieCard extends StatelessWidget {
   final String title;
+  final String img;
+  final int index;
+
   const MovieCard({
     super.key,
     required this.title,
+    required this.img,
+    required this.index,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10),
-      child: Text(
-        title,
-        style: const TextStyle(
-          overflow: TextOverflow.ellipsis,
-          color: Colors.white,
+    return GestureDetector(
+      onTap: () {
+        print('taped');
+      },
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width / 2,
+        child: Padding(
+          padding: index == 0
+              ? const EdgeInsets.only(
+                  left: 0,
+                  top: 10,
+                  bottom: 10,
+                  right: 5,
+                )
+              : const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(
+                img,
+                alignment: Alignment.center,
+                width: 150,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 5,
+                ),
+                child: Text(
+                  title,
+                  textAlign: TextAlign.start,
+                  style: const TextStyle(
+                    overflow: TextOverflow.ellipsis,
+                    color: Colors.white,
+                    letterSpacing: 1,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  maxLines: 2,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
